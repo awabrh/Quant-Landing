d84e76e0d4de85c6a302267ad1f0a6a7cc47da79
Awab Rhamtalla <awabrh@gmail.com>
2023-06-14 17:02:21 +0300

    added gradient to contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,13 +9,15 @@ export default function Contact() {
         DISCOVER BOUNDLESS EXCELLENCE!
       </h3>
       <p className="text-neutral-500 py-4">Get in touch with us.</p>
-      <div className="flex flex-col items-center px-10 py-6 rounded-2xl border-[1px] border-neutral-900">
-        <TextField name="name" placeholder="Name" />
-        <TextField name="email" placeholder="Email Address" type="email" />
-        <TextField name="company" placeholder="Company Name" />
-        <TextArea name="message" placeholder="Message" />
-        <div className="pt-5">
-          <Button>Send</Button>
+      <div className="bg-gradient-to-t from-[#001b72] to-neutral-900 rounded-2xl p-[0.7px]">
+        <div className="flex flex-col items-center px-10 py-6 rounded-2xl bg-black">
+          <TextField name="name" placeholder="Name" />
+          <TextField name="email" placeholder="Email Address" type="email" />
+          <TextField name="company" placeholder="Company Name" />
+          <TextArea name="message" placeholder="Message" />
+          <div className="pt-5">
+            <Button>Send</Button>
+          </div>
         </div>
       </div>
     </section>
