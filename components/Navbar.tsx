ef27d6a5a48871cce28b81c04348472686c27465
Awab Rhamtalla <awabrh@gmail.com>
2023-06-15 11:28:35 +0300

    added submenues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,10 @@ import Image from "next/image";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
+  const [arrowOffset, setArrowOffset] = useState("ml-[197px]");
+  const [bubbleOffset, setBubbleOffset] = useState("ml-[160px]");
+  const [bubbleVisability, setBubbleVisability] = useState("hidden");
+  const [isProducts, setIsProducts] = useState(true);
 
   return (
     <>
@@ -42,7 +46,12 @@ export default function Navbar() {
       ) : (
         <></>
       )}
-      <div className="flex items-center h-16 md:h-24 transition-all bg-black ">
+      <div
+        onMouseLeave={() => {
+          setBubbleVisability("hidden");
+        }}
+        className="flex items-center h-16 md:h-24 transition-all bg-black "
+      >
         <div className="flex justify-between w-full items-center mx-8">
           <Image
             src="/logo.png"
@@ -52,21 +61,74 @@ export default function Navbar() {
             className="hover:cursor-pointer"
           />
 
-          <div className="hidden md:flex">
+          <div className="hidden md:flex relative">
             <ul className="flex text-md gap-8 text-neutral-500 font">
               <li className="hover:text-white hover:cursor-pointer transition-all duration-200">
                 <Link href="/">Home</Link>
               </li>
               <li className="hover:text-white hover:cursor-pointer transition-all duration-200">
-                <Link href="/#">Enterprise</Link>
+                <Link
+                  href="/#"
+                  onMouseEnter={() => {
+                    setBubbleVisability("hidden");
+                  }}
+                >
+                  Enterprise
+                </Link>
               </li>
               <li className="hover:text-white hover:cursor-pointer transition-all duration-200">
-                <Link href="/#">Product</Link>
+                <Link
+                  href="/#"
+                  onMouseEnter={() => {
+                    setBubbleVisability("visible");
+                    setArrowOffset("ml-[197px]");
+                    setBubbleOffset("ml-[160px]");
+                    setIsProducts(true);
+                  }}
+                >
+                  Products
+                </Link>
               </li>
               <li className="hover:text-white hover:cursor-pointer transition-all duration-200">
-                <Link href="/#">Company</Link>
+                <Link
+                  href="/#"
+                  onMouseEnter={() => {
+                    setBubbleVisability("visible");
+                    setArrowOffset("ml-[297px]");
+                    setBubbleOffset("ml-[240px] w-28");
+                    setIsProducts(false);
+                  }}
+                >
+                  Company
+                </Link>
               </li>
             </ul>
+            <div
+              className={`absolute bg-neutral-500 rotate-45 mt-9 w-3 h-3 backdrop-blur ${arrowOffset} ${bubbleVisability} transition-all`}
+            ></div>
+            <div
+              className={`absolute flex flex-col p-4 gap-2 items-center text-neutral-500 border-[1px] border-neutral-500 mt-10 backdrop-blur rounded-lg ${bubbleOffset} ${bubbleVisability} transition-all`}
+            >
+              {isProducts ? (
+                <>
+                  <a className="hover:cursor-pointer hover:text-white transition-all">
+                    Suhail
+                  </a>
+                  <a className="hover:cursor-pointer hover:text-white transition-all">
+                    Fruit360
+                  </a>
+                </>
+              ) : (
+                <>
+                  <a className="hover:cursor-pointer hover:text-white transition-all block">
+                    About Us
+                  </a>
+                  <a className="hover:cursor-pointer hover:text-white transition-all">
+                    Careers
+                  </a>
+                </>
+              )}
+            </div>
           </div>
 
           <div
